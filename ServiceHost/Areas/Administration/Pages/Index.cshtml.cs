73c6db2c9e40a14cce5d763c3e70f931ab870b6f
M.A.Soltani <maxmadmas@gmail.com>
2022-03-16 21:34:18 +0330

    Admin Theme Added

---
+++ ServiceHost/Areas/Administration/Pages/Index.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceHost.Areas.Administration.Pages
+{
+    public class IndexModel : PageModel
+    {
+        
+        public void OnGet()
+        {
+
+        }
+    }
+}
